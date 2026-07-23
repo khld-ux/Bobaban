c3fabaf423cdf216037d45b1bba908c6da129df5
marwan.ai_ <marawanpablo884@gmail.com>
2026-07-23 05:19:18 +0300

    Create App component

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Bobaban - Watch Together</h1>
+      <p>Your collaborative video watching platform</p>
+    </div>
+  )
+}
+
+export default App
